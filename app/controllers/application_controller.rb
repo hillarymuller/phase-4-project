4943a6df7d45074d396b6e11100c2e2fe43b8ec2
Hillary Muller <hillaryaward@gmail.com>
2022-10-19 15:14:49 -0500

    add rescue from exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_authorized
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+wrap_parameters format: []
+
+private
+
+def render_not_authorized
+  render json: { error: "Not authorized" }, status: :unauthorized unless session.include?(:user_id)
+end
+def render_invalid(invalid)
+  render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+end
 
 end
