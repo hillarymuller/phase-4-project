e139563f5d122e284fbd86da076c74eec62b4109
Hillary Muller <hillaryaward@gmail.com>
2022-10-19 15:44:03 -0500

    add create & show to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+    def show
+        render json: @current_user
+    end
+    private
+    def user_params
+        params.permit(:name, :username, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   
   resources :chores
   resources :rooms
-  resources :users
   post "/signup", to: 'users#create'
   get '/me', to: 'users#show'
   post '/login', to: 'sessions#create'
