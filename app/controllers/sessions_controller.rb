26f4598eeccc69a869a6fdcd2d154d58f1365950
Hillary Muller <hillaryaward@gmail.com>
2022-10-19 15:36:24 -0500

    create & destroy methods sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: { error: "Username or password invalid" }, status: :unauthorized
+        end
+    end
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
 end
