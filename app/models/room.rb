23c337ea84bea5246ae0c490a343781524b1fc8d
Hillary Muller <hillaryaward@gmail.com>
2022-10-19 13:04:26 -0500

    add AR associations to models

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,4 @@
 class Room < ApplicationRecord
+    has_many :chores
+    has_many :users, through: :chores
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :chores
+    has_many :rooms, through: :chores
 end
