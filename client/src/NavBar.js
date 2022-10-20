744ff30317d9717f1105d5c93ce5e87817220cde
Hillary Muller <hillaryaward@gmail.com>
2022-10-20 11:03:17 -0500

    add options to navbar

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
-function NavBar() {
- 
+
+function NavBar({user}) {
+ return (
+    <div>
+        <NavLink 
+        to='/'>
+            Home
+            </NavLink>
+        {user ? (
+        <>
+        {/*<NavLink 
+        to='/user/:userId'>
+            My Chores
+        </NavLink>*/}
+        <NavLink 
+        to='/chores/new'>
+            Add New Chore
+        </NavLink>
+</> ) : (
+    <>
+        <NavLink to='/rooms'>
+            Rooms to Clean
+            </NavLink>
+            <NavLink to="/signup">
+                Sign Up
+            </NavLink>
+            <NavLink to='/login'>
+                Log In
+            </NavLink>
+            </>
+)}
+    </div>
+ )
 }
 
 export default NavBar;
