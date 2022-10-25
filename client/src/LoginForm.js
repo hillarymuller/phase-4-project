44d78cfa4f6e258a8fec03f4d2e003148177bb7c
Hillary Muller <hillaryaward@gmail.com>
2022-10-25 14:40:36 -0500

    login form

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function LoginForm() {
+function LoginForm({ handleLogin }) {
+const [username, setUsername] = useState("");
+
+    function handleChange(e){
+        setUsername(e.target.value)
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+        fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({username}),
+        })
+        .then(resp => {
+            if (resp.ok) {
+                resp.json()
+                .then(user => handleLogin(user))
+            }
+        });
+    }
+
  return (
-    <h2>Login</h2>
- )
+    <form onSubmit={handleSubmit}>
+        <label>
+            Username:
+            <input onChange={handleChange}
+                type="text"
+                name="username"
+                value={username}
+                />
+                <button type="submit">Login!</button>
+        </label>
+    </form>
+ );
 }
 
 export default LoginForm;
