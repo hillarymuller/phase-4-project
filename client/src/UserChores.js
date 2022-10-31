c463ee1111b80ecf3de828f5eb7d3ab46772f9c9
Hillary Muller <hillaryaward@gmail.com>
2022-10-31 17:13:44 -0500

    add edit button and mode to table

--- client/src/UserChores.js
+++ client/src/UserChores.js
@@ -4,6 +4,8 @@ import React, { useEffect, useState } from 'react';
 
 function UserChores() {
     const [chores, setChores] = useState([]);
+    const [editMode, setEditMode] = useState(false);
+    const [name, setName] = useState()
 
     useEffect(() => {
         fetch(`/mychores`)
@@ -47,8 +49,35 @@ function UserChores() {
         setChores(updatedChores);
     })
 }
+function handleEdit(chore) {
+    setEditMode(true);
+    console.log(chore)
+    setName(chore.name)
+}
+function handleChange(e) {
+    setName(e.target.value);
+}
+function handleSubmit(e) {
+    console.log(e);
+    setEditMode(false);
+}
     return (
+    
         <div>
+            {editMode ? (
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Chore: 
+                        <input onChange={handleChange}
+                        type="text"
+                        name="name"
+                        value={name}
+                        />
+                    </label>
+                    <button className="button" type="submit">Save</button>
+                </form>
+            ) : (
+                <>
         <h1>My Chores</h1>
         <table>
             <thead>
@@ -66,12 +95,14 @@ function UserChores() {
                             <td>{chore.name}</td>
                             <td>{chore.room.name}</td>
                             <td><button className="tbl-btn" onClick={() => handleStar(chore)}>{chore.starred ? "★" : "☆"}</button></td>
-                            <td><button className="tbl-btn" onClick={() => handleDelete(chore)}>All Done</button></td>
+                            <td><button className="tbl-btn" onClick={() => handleDelete(chore)}>Done</button></td>
+                            <td><button className="tbl-btn" onClick={() => handleEdit(chore)}>Edit</button></td>
                         </tr>
                     )
                 })}
             </tbody>
         </table>
+        </>)}
         </div>
     )
 }
